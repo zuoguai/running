f9dd1ba185870f21e01ce7ad1a23a13f1562e192
作怪 <2940505676@qq.com>
2024-03-29 21:55:04 +0800

    start

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: '世界末日',
+  siteUrl: 'https://zuoguai.git.io',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://zuoguai.github.io',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://baidu.com',
     },
   ],
 };
